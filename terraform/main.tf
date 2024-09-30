64f1893df8c8cded567cc9b3edebc50d12310aa9
Tomasz Tarczynski <tomek.taczynski@gmail.com>
2024-09-30 15:47:52 +0200

    AMI ID changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "allow_http_ssh" {
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  ami           = "ami-03cc8375791cb8bcf"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   key_name      = "tt_key"  # Your existing AWS key pair
 
