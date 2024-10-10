303741d61d3b1cfbc7b8cab20f04bff4a1cc7047
A <B>
2024-10-10 09:01:03 +0000

    Change

--- backend/sum.py
+++ backend/sum.py
@@ -1,6 +1,34 @@
 def sum(a, b):
-    
-    result=a+b +1+ 2
-    
-    
-    return(result)
+
+    s1 = "aaaa"
+    s2 = "bbbb"
+    l1 = [
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+        "aaaa",
+    ]
+
+    result = a + b + 1 + 2
+
+    return result
